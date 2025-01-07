157ea6bc6129a0dc180e3a13c438c05f70c1a739
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2025-01-07 18:31:41 +0100

    Added Common template project
    Contains some common interfaces and value types.

---
+++ src/Template/Common/ValueTypes/IValueType.cs
@@ -0,0 +1,21 @@
+﻿namespace Common.ValueTypes;
+
+/// <summary>
+/// Generic interface for value types wrapping some other value. 
+/// </summary>
+/// <typeparam name="TSelf">The value type.</typeparam>
+/// <typeparam name="TValue">The wrapped type.</typeparam>
+public interface IValueType<out TSelf, TValue> where TSelf : IValueType<TSelf, TValue>
+{
+    /// <summary>
+    /// The wrapped value.
+    /// </summary>
+    public TValue Value { get; }
+    
+    /// <summary>
+    /// Allows creation of the value type using its value.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>The value type.</returns>
+    public static abstract TSelf Create(TValue value);  
+}
